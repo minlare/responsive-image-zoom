6328556ad3c385499e87c492bb6d036dd4984c1c
Minlare <mike@outeredgeuk.com>
2014-04-03 13:01:07 +0100

    v2 - with jquery dependency

--- responsive-image-zoom.js
+++ responsive-image-zoom.js
@@ -1,61 +1,109 @@
 $.fn.responsiveImageZoom = function(){
     
-    var $el = $(this);
-    
-    var $offset = $(this).offset();
-    var $dimensions = {
-        width: $(this).width(),
-        height: $(this).height()
-    };
-    
-    var $img;
-    
-    
-    var moveZoomArea = function(e){
-        if(!$img) 
-            return;
-        
-        var horizontalPixelRatio = ($img.width() - $dimensions.width) / $dimensions.width;
-        var verticalPixelRatio = ($img.height() - $dimensions.height) / $dimensions.height;
-        
-        $img.css({
-            left: -((e.pageX - $offset.left) * horizontalPixelRatio),
-            top: -((e.pageY - $offset.top) * verticalPixelRatio)
-        });
-    };
-    
-    var zoomImage = function(e){
-        
-        $img = $(this);
-        if($img.data('zoomed')){
-            
-            $img.css({
-                maxWidth: '100%',
-                position: 'static',
-                top: 'auto',
-                left: 'auto'
-            });
-            $img.data('zoomed', false);
-        }
-        else{
-            // Set element dimensions so zooming is only shown inside original box
-            $el.css({
-                position: 'relative',
-                overflow: 'hidden',
-                height: $dimensions.height,
-                width: $dimensions.width
-            });
+    $(this).each(function(){
+
+        var $el = $(this),
+            $img,
+            $offset,
+            $dimensions = {},
+            $max = {},
+            $dragStart = {};
+
+        var zoomImage = function(e){
+
+            $offset = $el.offset();
+            $dimensions = {
+                width: $el.width(),
+                height: $el.height()
+            };
+
+            $img = $(e.target);
+            if($img.data('zoomed')){
+
+                // Return image back to original state
+                $img.css({
+                    maxWidth: '100%',
+                    position: 'static',
+                    top: 'auto',
+                    left: 'auto'
+                });
+                $img.data('zoomed', false);
+                $img.removeClass('responsive-image-zoom-active');
+                $img = false;
+            }
+            else{
+
+                // Set element dimensions so zooming is only shown inside original box
+                $el.css({
+                    position: 'relative',
+                    overflow: 'hidden',
+                    height: $dimensions.height,
+                    width: $dimensions.width
+                });
+
+                $img.css({
+                    maxWidth: 'none',
+                    position: 'absolute'
+                }).css({
+                    left: -($img.width() - $dimensions.width) / 2,
+                    top: -($img.height() - $dimensions.height) / 2
+                });
+                $img.data('zoomed', true);
+                $img.addClass('responsive-image-zoom-active');
+
+                $max = {
+                    left: parseInt(-($img.width() - $dimensions.width)),
+                    top: parseInt(-($img.height() - $dimensions.height))
+                };
+            }
+        };
+
+        var moveZoomArea = function(e){
+            if(!$img)
+                return;
+
+            var horizontalPixelRatio = ($img.width() - $dimensions.width) / $dimensions.width;
+            var verticalPixelRatio = ($img.height() - $dimensions.height) / $dimensions.height;
 
             $img.css({
-                maxWidth: 'none',
-                position: 'absolute',
-                top: 0,
-                left: 0
+                left: Math.max($max.left, -((e.pageX - $offset.left) * horizontalPixelRatio)),
+                top: Math.max($max.top, -((e.pageY - $offset.top) * verticalPixelRatio))
             });
-            $img.data('zoomed', true);
-        }
-    };
+        };
+
+        var startDragging = function(e){
+            if(!$img)
+                return;
+
+            $dragStart = {
+                left: parseInt($img.css('left')),
+                top: parseInt($img.css('top'))
+            };
+        };
+        var continueDragging = function(e){
+            if(!$img)
+                return;
+
+            $img.css({
+                left: Math.max($max.left, Math.min(0, e.gesture.deltaX + $dragStart.left)),
+                top: Math.max($max.top, Math.min(0, e.gesture.deltaY + $dragStart.top))
+            });
+        };
+
+        // All Devices
+        var $images = $(this).find('img');
+        $images.hammer().on('doubletap', zoomImage);
+
+        // Desktop
+        $(this).on('mousemove', moveZoomArea);
+
+        // Mobile
+        $(this).hammer().on('dragstart', startDragging);
+        $(this).hammer().on('drag', continueDragging);
+        $(document).on('touchmove', function(e){
+            if($img && $img.data('zoomed') && $(e.target).hasClass('responsive-image-zoom-active'))
+                e.preventDefault();
+        });
     
-    $(this).find('img').on('dblclick', zoomImage);
-    $(this).on('mousemove', moveZoomArea);
+    });
 };
